2d035d9d22150ded24d3d90daf6a498405c65e7e
YaraWS <39776511+YaraWS@users.noreply.github.com>
2023-08-25 23:21:42 +0100

    Add new dogs object

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const dogs = [
     {
         name: "Rex",
-        avatar: "images/dog-rex.png",
+        avatar: "images/dog-rex.jpg",
         age: 25,
         bio: "Art. Literature. Natural wine. Yoga.",
         hasBeenSwiped: false,
@@ -21,7 +21,55 @@ const dogs = [
         bio: "How you doin?",
         hasBeenSwiped: false,
         hasBeenLiked: false
-    }
+    },
+    {
+        name: "Tom",
+        avatar: "images/dog-book.gif",
+        age: 18,
+        bio: "Exploring the world, one book at a time. Coffee enthusiast and adventure seeker.",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Bruno",
+        avatar: "images/dog-cook.gif",
+        age: 18,
+        bio: "Chef by day, aspiring astronaut by night. Let's cook up some memories together!",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Luca",
+        avatar: "images/dog-ball.gif",
+        age: 18,
+        bio: "Nature lover and fitness enthusiast.",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Ronnie",
+        avatar: "images/dog-glasses.gif",
+        age: 30,
+        bio: "Laughter, and spontaneous road trips – let's make memories worth sharing.",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Brian",
+        avatar: "images/dog-running.gif",
+        age: 18,
+        bio: "Bookworm, beach lover, and believer in the power of a great conversation.",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Pierre",
+        avatar: "images/dog-pasta.gif",
+        age: 18,
+        bio: "I'm a big foodie, what's your passion?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
 ]
 
 export default dogs
